5bbc384ec9559ffde3590482dc5d90af867841bd
aslicicek475 <aslicicek475@gmail.com>
2026-05-02 23:43:59 +0300

    Refactor delivery recording and scoring methods

--- src/supplier.py
+++ src/supplier.py
@@ -6,19 +6,16 @@ class Supplier:
         self.id = str(uuid.uuid4())[:8]
         self.name = name
         self.total_deliveries = 0
-        self.approved_deliveries = 0
-        self.rejected_deliveries = 0
+        self.completed_deliveries = 0
 
-    def record_delivery(self, approved):
+    def record_delivery(self):
         self.total_deliveries += 1
 
-        if approved:
-            self.approved_deliveries += 1
-        else:
-            self.rejected_deliveries += 1
+    def record_completed_delivery(self):
+        self.completed_deliveries += 1
 
-    def quality_score(self):
+    def performance_score(self):
         if self.total_deliveries == 0:
             return 0
 
-        return (self.approved_deliveries / self.total_deliveries) * 100
+        return (self.completed_deliveries / self.total_deliveries) * 100
